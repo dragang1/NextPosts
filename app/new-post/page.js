a69adafcaa72bff47777b6c3866abaa0e3a04834
dragang1 <gajicdragan22@gmail.com>
2024-05-09 09:25:48 +0200

    Next Posts App..

---
+++ app/new-post/page.js
@@ -0,0 +1,14 @@
+
+import { createPost } from '@/actions/post';
+import PostForm from '@/components/post-form';
+
+
+export default function NewPostPage() {
+
+
+
+
+  return <PostForm action={createPost} />
+
+
+}
